1672650c5158de9d049eb9e4a28b3dc1fed682ee
laalarconf <lfernandeza@ucentral.edu.co>
2019-08-19 20:20:02 -0500

    Modificacion Interfaz y Documento Sql

--- src/main/webapp/Sql/base de datosProyecto.sql
+++ src/main/webapp/Sql/base de datosProyecto.sql
@@ -1,16 +1,13 @@
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 /**
  * Author:  Cesar
  * Created: 19/08/2019
  */
-CREATE TABLE ROL (idRol numeric(19, 0) NOT NULL, descripcionRol varchar(30) NOT NULL, PRIMARY KEY (idRol));
-CREATE TABLE USUARIO (idUsuario numeric(19, 0) NOT NULL, nomUsuario varchar(45), rolUsuario numeric(19, 0) NOT NULL, pasword varchar(30) NOT NULL, PRIMARY KEY (idUsuario));
-COMMENT ON COLUMN USUARIO.idUsuario IS 'Id usuario';
-COMMENT ON COLUMN USUARIO.nomUsuario IS 'nombre del usuario';
-COMMENT ON COLUMN USUARIO.rolUsuario IS 'id del rol que tiene el usuario';
-ALTER TABLE USUARIO ADD CONSTRAINT FKUSUARIO330298 FOREIGN KEY (rolUsuario) REFERENCES ROL (idRol);
+CREATE TABLE ROL (IdRol numeric(19, 0) NOT NULL, DescripcionRol varchar(30) NOT NULL, UsuarioAdicionado varchar(30) NOT NULL, FechaAdicionado Datetime NOT NULL PRIMARY KEY (IdRol));
+
+CREATE TABLE USUARIO (IdUsuario INT IDENTITY (1,1) PRIMARY KEY, DocumentoUsu varchar(45) NOT NULL,NomUsuario varchar(40) NOT NULL, ApeUsuario varchar(40) NOT NULL, IdrolUsu numeric(19, 0) NOT NULL, LoginUsu varchar(40) NOT NULL, ContrasenaUsu varchar(30) NOT NULL,UsuarioAdicionado varchar(30) NOT NULL, 
+					  FechaAdicionado Datetime NOT NULL);
+
+
+
 
